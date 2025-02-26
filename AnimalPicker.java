1c81647c774583801d3911af30904da3cae0c934
Jason Jackson <j.a.jackson1029@gmail.com>
2025-02-26 16:38:18 -0500

    User Input prompts and results

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -10,6 +10,27 @@ public class AnimalPicker {
         String animal;
         boolean choice = false;
 
+        while (!choice) {
+            System.out.println("Enter an animal.");
+            System.out.println();
+            String userAnimal = input.nextLine();
+            System.out.println();
+
+            switch (userAnimal.trim().toLowerCase()) {
+                case "dog":
+                    System.out.println(" DOG ASCII HERE");
+                    choice = true;
+                    break;
+                case "cat":
+                    System.out.println("CAT ASCII HERE");
+                    choice = true;
+                    break;
+                default:
+                    System.out.println("Invalid Animal");
+                    System.out.println();
+            }
+        }
+
     }
 
 }
