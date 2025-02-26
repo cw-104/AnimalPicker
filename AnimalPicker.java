ac6747fb8984de4faf2d5940f88665b0d3d4cf70
Jason Jackson <j.a.jackson1029@gmail.com>
2025-02-26 16:36:15 -0500

    Init

---
+++ AnimalPicker.java
@@ -0,0 +1,15 @@
+// J Jackson   
+// Animal Picker 
+
+import java.util.Scanner;
+
+public class AnimalPicker {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String animal;
+        boolean choice = false;
+
+    }
+
+}
